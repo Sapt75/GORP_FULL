2a875c37fcabea8facc2b8c459905cb9a4ceff5a
Sapt75 <109506771+Sapt75@users.noreply.github.com>
2024-01-23 11:02:52 +0530

    Inital Commit

--- router/auth.js
+++ router/auth.js
@@ -922,6 +922,9 @@ router.get('/filter/:type/:value/:state', async (req, res) => {
     filter[type] = { $regex: value, $options: 'i' }
 
 
+    console.log(filter, value)
+
+
     state == "true" ? next = 0 : null
 
 
@@ -1110,7 +1113,7 @@ router.get("/all_var/:type/:value", async (req, res) => {
     let { type, value } = req.params
     let filter = {}
     filter[type === "fuel_type" ? "Specifications.engine_and_transmission." + type: type] = value === "cng" || value === "muv" || value === "suv" ? value.toUpperCase() : value.charAt(0).toUpperCase() + value.slice(1)
-    let data = await CarData.find(filter).select("model_name Specifications Features brand fuel_type transmission_type -_id").limit(100)
+    let data = await CarData.find(filter).select("model_name Specifications Features brand fuel_type transmission_type -_id")
     console.log(data, filter)
     res.send(data)
 })
