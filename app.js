8f8cc8c0c073362d6aef833f07c8fbadbe6e77ae
Sapt75 <109506771+Sapt75@users.noreply.github.com>
2024-02-20 14:42:34 +0530

    Initial

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ require('./db/conn');
 // "mongodb+srv://admin:admin%40webshark@cluster0.mdurdqc.mongodb.net/onroadprice?retryWrites=true&w=majority"
 
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
 app.use(compression({
   level: 6,
   threshold: 0
@@ -30,6 +29,22 @@ app.use(body_parser.urlencoded({ extended: true, limit: "50mb" }))
 
 // const DB =`mongodb+srv://admin:admin%40webshark@cluster0.mdurdqc.mongodb.net/onroadprice?retryWrites=true&w=majority`;
 const PORT = process.env.PORT
+
+const whitelist = ['https://www.getonroadprice.com/'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      // Allow requests from whitelisted origins or requests with no origin (e.g., same-origin requests)
+      callback(null, true);
+    } else {
+      // Deny requests from origins not in the whitelist
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // Middleware
 const middleware = (req, res, next) => {
